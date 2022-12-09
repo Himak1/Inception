8ece2118895a6980041c7c2e910f92b5bc5d38cc
Justin Hille <jhille@f1r1s10.codam.nl>
2022-12-09 17:22:24 +0100

    mariadb starts up again in volume

--- Makefile
+++ Makefile
@@ -13,15 +13,15 @@ endif
 $(NAME): all
 
 all:
-	cd srcs ; $(DK_CMP) up 
+	cd srcs ; $(DK_CMP) up --build
 
 clean:
 	cd srcs ; $(DK_CMP) stop
 
 fclean: clean
 	cd srcs ; \
-	$(DK_CMP) rm ; \
-	docker image rm srcs_mariadb srcs_wordpress srcs_nginx ; \
+	$(DK_CMP) rm -f ; \
+	docker rmi $$(docker images -f "dangling=true" -q) ; \
 	docker volume rm srcs_database srcs_wordpress_web
 
 re: fclean all
--- srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++ srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -2,10 +2,10 @@
 # openrc
 # touch /run/openrc/softlevel
 # openrc
-mariadb-install-db --user=mysql --basedir=/dbdata --datadir=/dbdata/mariadb
-# rc-service mariadb start
+mariadb-install-db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
+# rc-service mariadb startdocker 
 # mariadb < set-users.sql
 sed -i s/root_password/$MYSQL_ROOT_PASSWORD/ set-users.sql
 sed -i s/new_user/$MYSQL_USER/ set-users.sql
 sed -i s/user_password/$MYSQL_PASSWORD/ set-users.sql
-exec mariadbd --user=mysql
+exec mariadbd --user=mysql < set-users.sql
